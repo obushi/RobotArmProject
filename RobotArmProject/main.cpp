4dd681b159e1a86c426fb301c5d181511d7af8ad
Noriyasu Obushi <nryas@me.com>
2018-04-19 16:55:27 +0900

    Initial Commit

---
+++ RobotArmProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RobotArmProject
+//
+//  Created by NoriyasuObushi on 2018/04/19.
+//  Copyright © 2018 NoriyasuObushi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
